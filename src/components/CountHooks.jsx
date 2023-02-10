9b60b09e0b314d371efad9231bfab31fd303d853
Gisele Abzalov <gisele.abzalov@gmail.com>
2023-02-10 10:08:50 -0500

    created class and functional components

---
+++ src/components/CountHooks.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export default function CountHooks() {
+    const [count, setCount] = useState(0)
+    const [title, setTitle] = useState('Welcome')
+    useEffect(() => {
+        setTitle('Our New Title')
+    }, [])
+    useEffect(() => {
+        document.title = `${count} clicks`
+    }, [count])
+
+    return (
+        <section>
+            <h2>{title}</h2>
+            <h3>You clicked {count} times.</h3>
+            <button onClick={() => setCount(count + 1)}>Click Me</button>
+        </section>
+    )
+    }
